32b714acf95801764058863f2d3b2278fe431981
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-05-16 14:51:30 -0400

    added cookies, login, and logout

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
 
 function generateRandomString() {
   let result = "";
@@ -15,6 +16,7 @@ function generateRandomString() {
 }
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -44,12 +46,13 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
  
- app.get("/fetch", (req, res) => {
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
 
- app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+app.get("/urls", (req, res) => {
+  console.log("cookies: ", req.cookies);
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
@@ -59,18 +62,12 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.post("/urls", (req, res) => {
-  let longURL = req.body.longURL;
-  let shortURL = generateRandomString(6);
-  urlDatabase[shortURL] = longURL;
-  res.redirect('urls'); // Respond with 'Ok' (we will replace this)
+  const templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -90,9 +87,28 @@ app.get("/urls/:id/edit", (req,res) => {
   res.redirect(`/urls/${urlID}`);
 });
 
+app.post("/urls", (req, res) => {
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = longURL;
+  res.redirect('urls'); // Respond with 'Ok' (we will replace this)
+});
+
 app.post("/urls/:id/edit", (req,res) => {
   const urlID = req.params.id;
   const newURL = req.body.longURL;
   urlDatabase[urlID] = newURL;
   res.redirect(`/urls/${urlID}`);
+});
+
+app.post("/login", (req,res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req,res) => {
+  const username = req.body.username;
+  res.clearCookie('username', username);
+  res.redirect("/urls");
 });
